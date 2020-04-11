4f81cea7407f56ec2b1e5d35a81f2e1cfc081cdf
Banks <iobg0320@gmail.com>
2020-04-11 12:19:02 -0400

    spaces to tabs

--- decompiler.c
+++ decompiler.c
@@ -16,61 +16,61 @@ int Disassemble8080Op(unsigned char *codebuffer, int pc){
 		// case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break; 
 		case 0x01: printf("LXI    B,#$%02x%02x", code[2], code[1]); opbytes=3; break;
 		//Store indirect (BC)
-        case 0x02: printf("STAX   B"); break;    
-        //increment register pair (BC)
-        case 0x03: printf("INX    B"); break;    
-        //increment register (B)
-        case 0x04: printf("INR    B"); break;    
-        //decrement register (B)
-        case 0x05: printf("DCR    B"); break;  
-        //move byte to register  
-        case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;    
-        //rotate A bits left
-        case 0x07: printf("RLC"); break;    
-        case 0x08: printf("NOP"); break; 
-        //16-bit register add
-        case 0x09: printf("DAD    B"); break; 
-        //load register pair to memory
-        case 0x0a: printf("LDAX   B"); break; 
-        //decrement register pair (BC)
-        case 0x0b: printf("DCX    B"); break; 
-    	case 0x0c: printf("INR    C"); break; 
-    	case 0x0d: printf("DCR    C"); break; 
-    	case 0x0e: printf("MVI    C,#$%02x", code[1]); opbytes=2; break;    
-    	//rotate A bits right
-        case 0x0f: printf("RRC"); break; 
+		case 0x02: printf("STAX   B"); break;    
+		//increment register pair (BC)
+		case 0x03: printf("INX    B"); break;    
+		//increment register (B)
+		case 0x04: printf("INR    B"); break;    
+		//decrement register (B)
+		case 0x05: printf("DCR    B"); break;  
+		//move byte to register  
+		case 0x06: printf("MVI    B,#$%02x", code[1]); opbytes=2; break;    
+		//rotate A bits left
+		case 0x07: printf("RLC"); break;    
+		case 0x08: printf("NOP"); break; 
+		//16-bit register add
+		case 0x09: printf("DAD    B"); break; 
+		//load register pair to memory
+		case 0x0a: printf("LDAX   B"); break; 
+		//decrement register pair (BC)
+		case 0x0b: printf("DCX    B"); break; 
+		case 0x0c: printf("INR    C"); break; 
+		case 0x0d: printf("DCR    C"); break; 
+		case 0x0e: printf("MVI    C,#$%02x", code[1]); opbytes=2; break;    
+		//rotate A bits right
+		case 0x0f: printf("RRC"); break; 
 
-        case 0x10: printf("NOP"); break;
-        case 0x11: printf("LXI    D,#$%02x%02x", code[2], code[1]); opbytes=3; break;
-        case 0x12: printf("STAX   D"); break;    
-        case 0x13: printf("INX    D"); break;    
-        case 0x14: printf("INR    D"); break;    
-        case 0x15: printf("DCR    D"); break;    
-        case 0x16: printf("MVI    D,#$%02x", code[1]); opbytes=2; break;  
-        //rotate A bits left through carry
-        case 0x17: printf("RAL"); break;    
-        case 0x18: printf("NOP"); break; 
-        case 0x19: printf("DAD    D"); break;
-        case 0x1a: printf("LDAX   D"); break;
-  		case 0x1b: printf("DCX    D"); break;
-  		case 0x1c: printf("INR    E"); break;
-  		case 0x1d: printf("DCR    E"); break;
-  		case 0x1e: printf("MVI    E,#$%02x", code[1]); opbytes=2; break;  
-  		//roate A bits right through carry
-  		case 0x1f: printf("RAR"); break;  
+		case 0x10: printf("NOP"); break;
+		case 0x11: printf("LXI    D,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+		case 0x12: printf("STAX   D"); break;    
+		case 0x13: printf("INX    D"); break;    
+		case 0x14: printf("INR    D"); break;    
+		case 0x15: printf("DCR    D"); break;    
+		case 0x16: printf("MVI    D,#$%02x", code[1]); opbytes=2; break;  
+		//rotate A bits left through carry
+		case 0x17: printf("RAL"); break;    
+		case 0x18: printf("NOP"); break; 
+		case 0x19: printf("DAD    D"); break;
+		case 0x1a: printf("LDAX   D"); break;
+		case 0x1b: printf("DCX    D"); break;
+		case 0x1c: printf("INR    E"); break;
+		case 0x1d: printf("DCR    E"); break;
+		case 0x1e: printf("MVI    E,#$%02x", code[1]); opbytes=2; break;  
+		//roate A bits right through carry
+		case 0x1f: printf("RAR"); break;  
 
-  		case 0x20: printf("NOP"); break; 
-  		case 0x21: printf("LXI    H,#$%02x%02x", code[2], code[1]); opbytes=3; break;
-  		//store (HL) to memory
+		case 0x20: printf("NOP"); break; 
+		case 0x21: printf("LXI    H,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+		//store (HL) to memory
 		case 0x22: printf("SHLD   $%02x%02x", code[2], code[1]); opbytes=3; break;
-        case 0x23: printf("INX    H"); break;
-        case 0x24: printf("INR    H"); break;
-        case 0x25: printf("DCR    H"); break;
-        case 0x26: printf("MVI    H,#$%02x", code[1]); opbytes=2; break;    
-        //Decimal adjust accumulator (????)
+		case 0x23: printf("INX    H"); break;
+		case 0x24: printf("INR    H"); break;
+		case 0x25: printf("DCR    H"); break;
+		case 0x26: printf("MVI    H,#$%02x", code[1]); opbytes=2; break;    
+		//Decimal adjust accumulator (????)
 		case 0x27: printf("DAA"); break;
-  		case 0x28: printf("NOP"); break; 
-        case 0x29: printf("DAD    H"); break;
+		case 0x28: printf("NOP"); break; 
+		case 0x29: printf("DAD    H"); break;
 		case 0x2a: printf("LHLD   $%02x%02x", code[2], code[1]); opbytes=3; break;
 		case 0x2b: printf("DCX    H"); break;
 		case 0x2c: printf("INR    L"); break;
